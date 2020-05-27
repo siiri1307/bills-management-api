64115d15fd2095888dfafab4312c26d1bd722f34
Siiri Saar <s.saar@axinom.com>
2020-05-27 23:06:26 +0300

    Renamed controller, updated cors policy

--- korteriyhistu/Startup.cs
+++ korteriyhistu/Startup.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using korteriyhistu.Models;
 using DinkToPdf.Contracts;
@@ -38,7 +32,7 @@ namespace korteriyhistu
                 options.AddPolicy(name: BillsFEAppOrigins,
                     builder => {
                         builder.WithOrigins("https://bills-management-app.herokuapp.com", "http://localhost:4200")
-                        .AllowAnyHeader();
+                        .AllowAnyHeader().AllowAnyMethod();
                     });
             });
 
@@ -84,7 +78,6 @@ namespace korteriyhistu
                     options.UseSqlServer(Configuration.GetConnectionString("LogEntriesContext")));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
             if (isDev)
